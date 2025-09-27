6e6bae2462af1df8ca2a02728e60cc47203b794e
AvramenkoD <dsdautor227@gmail.com>
2025-09-27 20:48:26 +0300

    Добавьте файлы проекта.

---
+++ source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	float aSide;
+	float bSide;
+	float perimetr;
+	float square;
+
+	setlocale(LC_ALL, "RUS");
+	puts("Введите сторону А...");
+	scanf("%f", &aSide);
+	puts("Введите сторону В...");
+	scanf("%f", &bSide);
+
+	perimetr = (aSide + bSide) * 2;
+	square = aSide * bSide;
+
+	printf("У прямоугольника со сторонами %g и %g:\n", aSide, bSide);
+	printf("Периметр равен (%0.1f + %0.1f) * 2 = %0.1f\n", aSide, bSide, perimetr);
+	printf("Площадь равна %0.1f * %0.1f = %0.1f\n", aSide, bSide, square);
+
+	return 0;
+}
